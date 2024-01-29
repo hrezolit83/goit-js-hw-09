a1f715446cd07cf7c85daec35d65f92641b5cd85
hrezolit83 <110486214+hrezolit83@users.noreply.github.com>
2024-01-29 13:10:56 +0000

    Deploying to gh-pages from @ hrezolit83/goit-js-hw-09@4d93ced19abc1271abc3d59d49c0c0bd2d6f71f4 🚀

--- assets/2-form-4dd2cbaa.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form"),s="feedback-form-state",t=JSON.parse(localStorage.getItem(s));t&&(e.elements.email.value=t.email||"",e.elements.message.value=t.message||"");e.addEventListener("input",l=>{const a={email:e.elements.email.value,message:e.elements.message.value};localStorage.setItem(s,JSON.stringify(a))});e.addEventListener("submit",l=>{l.preventDefault();const a={email:e.elements.email.value,message:e.elements.message.value};console.log(a),localStorage.removeItem(s),e.reset()});
-//# sourceMappingURL=2-form-4dd2cbaa.js.map
---
+++ assets/2-form-c460ba34.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form"),m="feedback-form-state",a=JSON.parse(localStorage.getItem(m));a&&(e.elements.email.value=a.email,e.elements.message.value=a.message);e.addEventListener("input",s=>{const t={email:e.elements.email.value.trim(),message:e.elements.message.value.trim()};localStorage.setItem(m,JSON.stringify(t))});e.addEventListener("submit",s=>{s.preventDefault();const t={email:e.elements.email.value.trim(),message:e.elements.message.value.trim()};console.log(t),localStorage.removeItem(m),e.reset()});
+//# sourceMappingURL=2-form-c460ba34.js.map
